c18cc00f3892ae0eafcff5364d98d52c3c2805be
Unknown <adriian.-@live.com>
2018-07-11 11:39:38 -0400

    mlp en proceso

--- mlp_additive_forecasting.m
+++ mlp_additive_forecasting.m
@@ -3,11 +3,11 @@ function [parameters] = mlp_additive_forecasting(x_test_hf,y_test_hf,x_test_lf,y
   W_hf=bestWTrainGlobal;
   load("mejorTopologiaMLP_hf.txt");
   topologia_hf=topologia;
+  y_hf=mlp_test_fp(x_test_hf,W_hf);
   load("mejorPesoMLP_lf.txt");
   W_lf=bestWTrainGlobal;
   load("mejorTopologiaMLP_lf.txt");
   topologia_lf=topologia;
-  y_hf=mlp_test_fp(x_test_hf,W_hf);
   y_lf=mlp_test_fp(x_test_lf,W_lf);
   y_additive=y_hf+y_lf;
 endfunction
