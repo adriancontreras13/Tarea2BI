0ae0b8811abf1a1b9b156f3a84a5a5312f0d5dc6
Unknown <adriian.-@live.com>
2018-07-21 18:22:45 -0400

    correccion 2 svm

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ arreglo_hankel=[2,4,6,8,10,15,20,30,40,50];
 %Rango de las h del Horizonte
 arreglo_h=[20];
 %Arreglo de lags
-lag=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+lag=[30];
 %Datasets
 data= csvread("dataeval.csv");
 %data= csvread("daily-minimum-temperatures-in-me.csv");
--- svm_test.m
+++ svm_test.m
@@ -2,5 +2,4 @@ function [y]= svm_test(alpha, beta, trainx, testing, s)
   for i=1:rows(testing)
     y(end+1,1) =alpha'  * kernel(trainx,testing(i,:),s) + beta;
   end
-  disp(y(end));
 end
